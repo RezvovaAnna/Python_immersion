2e11cd2c05d04b69fb64347456ca1b75766c9de2
Anna_Rezvova <rezvova.a@mail.ru>
2023-09-25 20:18:18 +0500

    Add solution of task 1,2,3 sem3

--- sem3_task03.py
+++ sem3_task03.py
@@ -2,3 +2,16 @@
 # Определите какие вещи влезут в рюкзак передав его максимальную грузоподъёмность.
 # Достаточно вернуть один допустимый вариант.
 # *Верните все возможные варианты комплектации рюкзака.
+
+
+dict_object = {'палатка': 12, 'термос': 2, 'вода': 6, 'спальный мешок': 9,
+               'набор продуктов': 5, 'аптечка': 1, 'одежда': 3, 'гитара': 4}
+backpack_capacitys = int(input('Введите вместимость рюкзака: '))
+var_backpack = dict(sorted(dict_object.items(), key=lambda x: -x[1]))
+print(var_backpack)
+contents_backpack = []
+for i, k in var_backpack.items():
+    if k <= backpack_capacitys:
+        contents_backpack.append(i)
+        backpack_capacitys -= k
+print(f'Можно положить в рюкзак - {contents_backpack}')
